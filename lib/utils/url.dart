07aa9f86bc15683139ea56f88c6fe50743673597
Fharza Yusuf <fharzayusuf@gmail.com>
2022-05-25 12:06:08 +0700

    update key

--- lib/utils/url.dart
+++ lib/utils/url.dart
@@ -1,5 +1,5 @@
 class Api {
   static const String base_url = "https://newsapi.org/v2/"; // base url
   static const String newsapi_key =
-      "3bdbfa38eff140a6bbab26ad651c9db7"; // api key
+      "3bdbfa38eff140a6bbab"; // add your api key newapi here
 }
